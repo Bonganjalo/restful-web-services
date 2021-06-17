3939f50f1c7cc6e26c7957597407cb97f8495afc
Bonganjalo Hadebe <bonganjaloh.hadebe@gmail.com>
2021-06-17 23:51:59 +0200

    added HATEOAS

--- src/main/java/com/in28minutes/rest/webservices/user/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/user/UserController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.rest.webservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -9,32 +11,38 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 @RestController
 public class UserController {
     @Autowired
     private UserService service;
 
 
-    // GET
-    // retrieveAllUsers
-
+    // GET /users
     @GetMapping(path = "/users")
-    public List<User> retriveAllUuers(){
+    public List<User> retriveAllUsers(){
       return service.findAll();
     }
 
     //GET /users/{id}
     @GetMapping(path = "/users/{id}")
-    public User retriveAllUuers(@PathVariable int id){
+    public EntityModel<User> retriveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if(user == null){
             throw new UserNotFoundException("id-" + id);
         }
+        EntityModel<User> model = EntityModel.of(user);
 
-        return user;
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retriveAllUsers());
+
+        model.add(linkToUsers.withRel("all-users"));
+
+        return model;
     }
 
+    // POST /users
     @PostMapping("/users")
     public ResponseEntity createUser(@Validated @RequestBody User user){
         User savedUser = service.save(user);
@@ -45,7 +53,7 @@ public class UserController {
         return ResponseEntity.created(location).build();
     }
 
-    //DELET /users/{id}
+    //DELETE /users/{id}
     @DeleteMapping(path = "/users/{id}")
     public User deleteUser(@PathVariable int id){
         User user = service.deleteUserById(id);
